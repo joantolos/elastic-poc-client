3c9e0c1cc1b3ee94bbd4169af40bbd8d1b316732
jtolos <Mrfalken3>
2015-12-31 10:55:35 +0100

    Placeholder and entities

--- src/main/java/com/joantolos/poc/elastic/ElasticDriver.java
+++ src/main/java/com/joantolos/poc/elastic/ElasticDriver.java
@@ -2,6 +2,8 @@ package com.joantolos.poc.elastic;
 
 public class ElasticDriver {
 
+    private final String ELASTIC_URL = "https://4udfe4e3:8p85z4at2nxnhvsa@maple-563900.us-east-1.bonsai.io";
+
     public void createIndex(String indexName){
 
     }
--- src/main/java/com/joantolos/poc/elastic/entity/Planet.java
+++ src/main/java/com/joantolos/poc/elastic/entity/Planet.java
@@ -4,14 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Planet {
+
     Long id;
     String name;
     Mythology mythology;
-
 
 }
