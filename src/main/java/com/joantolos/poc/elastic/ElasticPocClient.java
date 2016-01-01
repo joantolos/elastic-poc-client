45a663d9220cf397f60501a128f0260d3e53f571
Joan Tolos <joan.tolos@thomsonreuters.com>
2016-01-01 19:21:37 +0100

    CRUD operations on Elastic Search implemented

--- src/main/java/com/joantolos/poc/elastic/ElasticPocClient.java
+++ src/main/java/com/joantolos/poc/elastic/ElasticPocClient.java
@@ -3,14 +3,14 @@ package com.joantolos.poc.elastic;
 import com.joantolos.poc.elastic.engine.ElasticSearchEngine;
 import com.joantolos.poc.elastic.utils.FileUtils;
 
-public class SetUpElastic {
+public class ElasticPocClient {
 
     public static void main (String[] args) {
 
         ElasticSearchEngine elasticSearchEngine = new ElasticSearchEngine();
         String index = "planets";
-        String mappings = FileUtils.streamToString(SetUpElastic.class.getClassLoader().getResourceAsStream("planetsIndexCreation.json"));
-        String bulk = FileUtils.streamToStringWithNewLineChar(SetUpElastic.class.getClassLoader().getResourceAsStream("firstTwoPlanets.bulk"));
+        String mappings = FileUtils.streamToString(ElasticPocClient.class.getClassLoader().getResourceAsStream("planetsIndexCreation.json"));
+        String bulk = FileUtils.streamToStringWithNewLineChar(ElasticPocClient.class.getClassLoader().getResourceAsStream("firstTwoPlanets.bulk"));
 
         elasticSearchEngine.delete(index);
         elasticSearchEngine.mappings(index, mappings);
